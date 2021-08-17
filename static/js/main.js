20d04c99f54fe4adcad3cc8052412800bbe96493
RoryBr1 <rorybr1@gmail.com>
2021-08-17 14:33:21 +0000

    navbar complete

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+/* Variables */
+var updateBtns = document.getElementsByClassName('update-cart')
+
+/* Toggle navbar-toggler icon between "bars" and "X" icons 
+    according to whether the mobile navbar is open */
+$(function () {
+    $('#navbarSupportedContent')
+        /* When mobile navbar is open, hide the "bars" icon and display the "X" icon */
+        .on('shown.bs.collapse', function () {
+            $('#navbar-open-icon').addClass('hidden');
+            $('#navbar-close-icon').removeClass('hidden');
+        })
+        /* When mobile navbar is closed, hide the "X" icon and display the "bars" icon */
+        .on('hidden.bs.collapse', function () {
+            $('#navbar-open-icon').removeClass('hidden');
+            $('#navbar-close-icon').addClass('hidden');
+        });
+
+});
--- techzone/settings.py
+++ techzone/settings.py
@@ -159,7 +159,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
