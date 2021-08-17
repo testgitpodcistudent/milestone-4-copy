81524f59f2193834efa8106a82a9b52ae0b7c903
RoryBr1 <rorybr1@gmail.com>
2021-08-17 13:30:08 +0000

    added links to navbar

--- techzone/urls.py
+++ techzone/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts', include('allauth.urls')),
 ]
