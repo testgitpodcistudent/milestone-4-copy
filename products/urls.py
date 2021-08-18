31a3db1399d493ea10b28051654144bc60ec539c
RoryBr1 <rorybr1@gmail.com>
2021-08-18 21:43:10 +0000

    main products page and product detail page functional

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_page, name='product_page'),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product
 
-# Create your views here.
+# All Products
+def all_products(request):
+    """ display all products; includes sorting, search query functionality """
+    
+    products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter a search term")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+    
+    return render(request, 'products/products.html', context)
+
+
+def product_page(request, product_id):
+    """ display individual product detail page """
+    
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    
+    return render(request, 'products/product_page.html', context)
