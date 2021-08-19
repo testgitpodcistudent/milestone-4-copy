c9a2d7418285957e43c420c1f7d1d6e63e37dd1b
RoryBr1 <rorybr1@gmail.com>
2021-08-19 22:47:06 +0000

    added add_to_cart functionality; needs to be rendered in templates

--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
-from django.shortcuts import render
 
 # Create your views here.
 def view_cart(request):
     """ return cart contents page """
-    
+
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ add 1 of the specified product to the cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
