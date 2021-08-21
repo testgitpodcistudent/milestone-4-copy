d26b28725e6efd1b7e241e5e5fa1f37d734f3d6a
RoryBr1 <rorybr1@gmail.com>
2021-08-21 13:11:56 +0000

    added cart preview to success messages

--- cart/views.py
+++ cart/views.py
@@ -23,10 +23,10 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
-        messages.success(request, f'Added {product.name} to your bag')
+        messages.success(request, f'Added "{product.name}" to your bag')
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your bag')
+        messages.success(request, f'Added "{product.name}" to your bag')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -41,9 +41,10 @@ def adjust_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] = quantity
+        messages.success(request, f'Updated "{product.name}" quantity to [{item_id}]')
     else:
         cart[item_id] = quantity
-        messages.info(request, f'Added {product.name} to your bag')
+        messages.success(request, f'Updated "{product.name}" quantity to [{item_id}]')
 
 
     request.session['cart'] = cart
@@ -51,11 +52,16 @@ def adjust_cart(request, item_id):
 
 
 def remove(request, item_id):
+
+    product = get_object_or_404(Product, pk=item_id)
+
     """ remove all of specified item from the cart """
     try:
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        
+        messages.info(request, f'Removed "{product.name}" from bag')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
