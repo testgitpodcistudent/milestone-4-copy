bf043df30789c4c398f1aec02bf872890b095a39
RoryBr1 <rorybr1@gmail.com>
2021-08-21 10:30:06 +0000

    cart subtotal working. adjust_cart (update and remove cart items) working.

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, reverse, HttpResponse
 
 
 # Create your views here.
@@ -22,3 +22,32 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """ adjust quantity of specified product in cart to a specified amount from cart.html """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] = quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse("view_cart"))
+
+
+def remove(request, item_id):
+    """ remove all of specified item from the cart """
+    try:
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
