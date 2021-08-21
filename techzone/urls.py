7bbb9363c1d4618acc26c3e9fb1274221b93c153
RoryBr1 <rorybr1@gmail.com>
2021-08-21 20:28:46 +0000

    started checkout app logic

--- techzone/urls.py
+++ techzone/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('accounts', include('allauth.urls')),
+
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
-    path('accounts', include('allauth.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
