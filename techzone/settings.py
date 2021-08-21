4abc0a687de6bb8e961e33b7219de5969bc81f87
RoryBr1 <rorybr1@gmail.com>
2021-08-21 22:10:43 +0000

    checkout and cart styling

--- techzone/settings.py
+++ techzone/settings.py
@@ -113,7 +113,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.contexts.cart_contents'
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
