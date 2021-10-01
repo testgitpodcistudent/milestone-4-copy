ae6846ff4d93e1eca5022a6c09004f640f4d8387
RoryBr1 <rorybr1@gmail.com>
2021-10-01 20:10:02 +0000

    created contact/forms.py and ContactForm class within

---
+++ contact/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+# Contact Form
+class ContactForm(forms.Form):
+	name = forms.CharField(max_length = 50)
+	email_address = forms.EmailField(max_length = 150)
+    message = forms.CharField(widget = forms.Textarea, max_length=2000)
