a61718fad99a3c0f73b6fdabacec46ef8fe3f78a
RoryBr1 <rorybr1@gmail.com>
2021-10-02 18:42:36 +0000

    Removed redundant header code from checkout.html. Added logic to contact/views.py. Contact form is not functional yet.

--- contact/forms.py
+++ contact/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
 	name = forms.CharField(max_length = 50)
 	email_address = forms.EmailField(max_length = 150)
-    message = forms.CharField(widget = forms.Textarea, max_length=2000)
+	message = forms.CharField(widget = forms.Textarea, max_length=2000)
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
 def contact(request):
-    """return contact page"""
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry" 
+            body = {
+            'name': form.cleaned_data['name'],
+            'email_address': form.cleaned_data['email_address'], 
+            'message':form.cleaned_data['message'], 
+            }
+            message = "\n".join(body.values())
 
-    return render(request, "contact/contact.html")
+            try:
+                send_mail(subject, message, 'codeinstitutestudent1@gmail.com', ['codeinstitutestudent1@gmail.com']) 
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect ("main:homepage")
+
+    form = ContactForm()
+    return render(request, "main/contact.html", {'form':form})
