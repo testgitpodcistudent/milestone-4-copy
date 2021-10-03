8f007f69c1218198868a046add1526b954fa2571
RoryBr1 <rorybr1@gmail.com>
2021-10-03 12:04:18 +0000

    Line splitting in settings.py on DATABASES causing server error. Temporarily unsplit lines to test.

--- techzone/settings.py
+++ techzone/settings.py
@@ -126,9 +126,7 @@ WSGI_APPLICATION = "techzone.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.parse(
-        ("postgres://qwmrksyzdlafcq:ada670ddcadf8\
-            195faa35f5c9aedd63670367ef00a21f9922323737a87a7ad7f@ec2-54-1\
-                54-101-45.eu-west-1.compute.amazonaws.com:5432/ddmdbob5jch9qd")
+        ("postgres://qwmrksyzdlafcq:ada670ddcadf8195faa35f5c9aedd63670367ef00a21f9922323737a87a7ad7f@ec2-54-154-101-45.eu-west-1.compute.amazonaws.com:5432/ddmdbob5jch9qd")
     )
 }
 
