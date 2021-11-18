a490ae4433a4c95dcd203479a17d876dbba3a181
RoryBr1 <rorybr1@gmail.com>
2021-11-18 19:47:53 +0000

    fixed 'line too long' errors techzone/settings.py

--- techzone/settings.py
+++ techzone/settings.py
@@ -140,16 +140,20 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': ('django.contrib.auth.password_
+                 validation.UserAttributeSimilarityValidator',)
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': ('django.contrib.auth.passw
+                 ord_validation.MinimumLengthValidator',)
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': ('django.contrib.auth.passwo
+                 rd_validation.CommonPasswordValidator',)
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': ('django.contrib.auth.password_va
+                 lidation.NumericPasswordValidator',)
     },
 ]
 
