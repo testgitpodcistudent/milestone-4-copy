41db9c6ef61122f96dbcf7073aa671e265644095
RoryBr1 <rorybr1@gmail.com>
2021-11-18 21:32:12 +0000

    reverted checkout/views.py to remove if/else statement regarding AnonymousUser as the intended fix was not working

--- checkout/views.py
+++ checkout/views.py
@@ -163,10 +163,8 @@ def checkout_success(request, order_number):
     save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
 
-    if request.user.is_anonymous():
-        profile = None
-    else:
-        profile = UserProfile.objects.get(user=request.user)
+    profile = UserProfile.objects.get(user=request.user)
+    
     # Attach the user's profile to the order
     order.user_profile = profile
     order.save()
