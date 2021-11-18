caf7cb94d934746998ae2bef3018deeb773228b4
RoryBr1 <rorybr1@gmail.com>
2021-11-18 21:26:22 +0000

    fixing SyntaxError checkout/views.py line 166 (request.user.is_anonymous is a method)

--- checkout/views.py
+++ checkout/views.py
@@ -163,7 +163,7 @@ def checkout_success(request, order_number):
     save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
 
-    if request.user.is_anonymous = True:
+    if request.user.is_anonymous():
         profile = None
     else:
         profile = UserProfile.objects.get(user=request.user)
