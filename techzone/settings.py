603d1bad8e436b6a126263b3231296f0e2989e3a
RoryBr1 <rorybr1@gmail.com>
2021-11-20 18:07:20 +0000

    Unsplit lines from 143-156 in settings.py which were causing django errors.

--- techzone/settings.py
+++ techzone/settings.py
@@ -142,20 +142,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': ('django.contrib.auth.passwor\
-        d_validation.UserAttributeSimilarityValidator',)
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': ('django.contrib.auth.passwo\
-        rd_validation.MinimumLengthValidator',)
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': ('django.contrib.auth.password_vali\
-        dation.CommonPasswordValidator',)
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': ('django.contrib.auth.passwor\
-        d_validation.NumericPasswordValidator',)
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
